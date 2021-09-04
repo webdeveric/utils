39b1da357721fb866fa920679271f7050c10e764
Eric King <eric@webdeveric.com>
2021-09-04 13:33:14 -0500

    Updated delay()
    - Reject promise with TypeError when given invalid milliseconds value

--- src/delay.ts
+++ src/delay.ts
@@ -1,8 +1,20 @@
-export function delay<T>(ms: number, value?: T) : Promise<typeof value>
+/**
+ * Delay a specified number of milliseconds before resolving.
+ * The `ms` value can be between zero and the max delay value for `setTimeout`.
+ *
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value}
+ */
+export function delay<T>(milliseconds: number, value?: T) : Promise<T>
 {
-  if ( ! Number.isInteger( ms ) || ms < 0 ) {
-    throw new Error('ms must be a positive integer');
-  }
+  return new Promise( (resolve, reject) => {
+    if ( typeof milliseconds !== 'number' ) {
+      const providedType = Object.prototype.toString.call(milliseconds).match(/\[\w+\s(?<type>.+)\]/)?.groups?.type;
 
-  return new Promise( resolve => setTimeout( resolve, ms, value ) );
+      reject( new TypeError(`milliseconds must be a number. ${providedType} was provided.`) );
+
+      return;
+    }
+
+    setTimeout( resolve, Math.max(0, Math.min(milliseconds, 2_147_483_647)), value );
+  } );
 }
--- test/delay.spec.js
+++ test/delay.spec.js
@@ -6,7 +6,7 @@ describe('delay()', () => {
   });
 
   it('Delays by some number of milliseconds', async () => {
-    const tenSecondDelay = delay(10000);
+    const tenSecondDelay = delay(10_000);
 
     jest.runOnlyPendingTimers();
 
@@ -14,16 +14,14 @@ describe('delay()', () => {
   });
 
   it('Specifies the return value', async () => {
-    const tenSecondDelay = delay(10000, 'value');
+    const tenSecondDelay = delay(10_000, 'value');
 
     jest.runOnlyPendingTimers();
 
     await expect( tenSecondDelay ).resolves.toBe('value');
   });
 
-  it('Throws when provided invalid ms', () => {
-    expect( () => {
-      delay('wrong ms value');
-    }).toThrow();
+  it('Rejects when provided invalid milliseconds', async () => {
+    await expect( delay('wrong milliseconds value') ).rejects.toThrow();
   });
 });
