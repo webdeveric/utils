83150b4baeedd443f778dbc5aa903e8c772d40c7
Eric King <eric@webdeveric.com>
2022-01-19 14:18:40 -0600

    Added simple script to validate you can `require()` and `import()` this package.

---
+++ bin/validate.cjs
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+async function validate(name) {
+  console.group(`Validating ${name}`);
+
+  const checks = new Map();
+
+  checks.set(
+    `require('${name}');`,
+    () => {
+      require(name);
+    },
+  );
+
+  checks.set(
+    `import('${name}');`,
+    async () => {
+      await import(name);
+    },
+  );
+
+  for (const [ name, fn ] of checks) {
+    try {
+      await fn();
+
+      console.log(`✅ ${name}`);
+    } catch (error) {
+      console.log(`❌ ${name}`);
+      console.error(error);
+
+      process.exitCode = 1;
+    }
+  }
+
+  console.groupEnd();
+}
+
+if (typeof process.env.npm_package_name === 'string') {
+  validate(process.env.npm_package_name);
+} else {
+  console.error('Cannot find npm package name in your environment.');
+  console.info('Run this script from your package.json scripts block.');
+  console.info('Example: npm run validate');
+}
