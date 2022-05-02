0dfa0807fe280e379a65488526a66101a7d451aa
Eric King <eric@webdeveric.com>
2022-05-02 12:18:19 -0500

    Check `size` property of `Set` and `Map`

--- test/isEmpty.test.ts
+++ test/isEmpty.test.ts
@@ -7,13 +7,19 @@ describe('isEmpty()', () => {
     expect(isEmpty({})).toBeTruthy();
     expect(isEmpty(null)).toBeTruthy();
     expect(isEmpty(undefined)).toBeTruthy();
+    expect(isEmpty(new Set())).toBeTruthy();
+    expect(isEmpty(new Map())).toBeTruthy();
+    expect(isEmpty({ length: 0 })).toBeTruthy();
   });
 
   it('Returns false for non-empty values', () => {
     expect(isEmpty('test')).toBeFalsy();
-    expect(isEmpty([ 'test' ])).toBeFalsy();
+    expect(isEmpty(['test'])).toBeFalsy();
     expect(isEmpty({ test: true })).toBeFalsy();
     expect(isEmpty(0)).toBeFalsy();
     expect(isEmpty(Math.PI)).toBeFalsy();
+    expect(isEmpty(new Set(['test']))).toBeFalsy();
+    expect(isEmpty(new Map([['test', true]]))).toBeFalsy();
+    expect(isEmpty({ length: 10 })).toBeFalsy();
   });
 });
