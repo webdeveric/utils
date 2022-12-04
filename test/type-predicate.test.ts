72381412d92b012665c5ecec9afe4dd27fbef760
Eric King <eric@webdeveric.com>
2022-12-04 15:25:04 -0600

    Updated type predicates
    - Added more predicate functions
    - Updated types
    - Updated tests

--- test/type-predicate.test.ts
+++ test/type-predicate.test.ts
@@ -1,30 +1,276 @@
 import {
+  createIsEnumPredicate,
+  isBigInt,
+  isBoolean,
   isFiniteNumber,
   isInteger,
   isIntString,
   isLengthAware,
+  isNull,
+  isNumber,
   isNumericString,
   isNumericValue,
   isNumericValueArray,
   isObject,
+  isOptionalBigInt,
+  isOptionalBoolean,
+  isOptionalNull,
   isOptionalNumber,
   isOptionalString,
+  isOptionalSymbol,
   isPositiveFiniteNumber,
   isPositiveInteger,
   isPrimitive,
+  isPromiseFulfilledResult,
+  isPromiseRejectedResult,
   isPropertyKey,
+  isString,
   isStringMatching,
-} from '../src/type-predicate';
+  isStringRecord,
+  isStringWithLength,
+  isSymbol,
+  isUndefined,
+  maybeUndefined,
+} from '../src/type-predicate.js';
 
-describe('isFiniteNumber()', () => {
+describe('createIsEnumPredicate()', () => {
+  enum Demo {
+    Demo = 'Demo',
+  }
+
+  it('Returns a type predicate function', () => {
+    expect(createIsEnumPredicate(Demo)).toBeInstanceOf(Function);
+  });
+
+  it('Predicate function checks if input is a member of the enum', () => {
+    const fn = createIsEnumPredicate(Demo);
+
+    expect(fn(Demo.Demo)).toBeTruthy();
+    expect(fn('Fail')).toBeFalsy();
+  });
+});
+
+describe('maybeUndefined()', () => {
+  it('Calls a type predicate function', () => {
+    const fn = jest.fn(isBoolean);
+
+    expect(maybeUndefined(fn as unknown as typeof isBoolean, true)).toBeTruthy();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('isString()', () => {
   it('Returns true for valid input', () => {
-    expect(isFiniteNumber(1)).toBeTruthy();
+    expect(isString('test')).toBeTruthy();
+    expect(isString(undefined)).toBeFalsy();
+  });
+});
+
+describe('isNumber()', () => {
+  it('Returns true for valid input', () => {
+    expect(isNumber(Math.PI)).toBeTruthy();
+    expect(isNumber(undefined)).toBeFalsy();
+  });
+});
+
+describe('isBigInt()', () => {
+  it('Returns true for valid input', () => {
+    expect(isBigInt(BigInt(1))).toBeTruthy();
+    expect(isBigInt(undefined)).toBeFalsy();
+  });
+});
+
+describe('isBoolean()', () => {
+  it('Returns true for valid input', () => {
+    expect(isBoolean(true)).toBeTruthy();
+    expect(isBoolean(undefined)).toBeFalsy();
+  });
+});
+
+describe('isUndefined()', () => {
+  it('Returns true for valid input', () => {
+    expect(isUndefined(undefined)).toBeTruthy();
+    expect(isUndefined(1)).toBeFalsy();
+  });
+});
+
+describe('isSymbol()', () => {
+  it('Returns true for valid input', () => {
+    expect(isSymbol(Symbol.for('test'))).toBeTruthy();
+    expect(isSymbol(1)).toBeFalsy();
+  });
+});
+
+describe('isNull()', () => {
+  it('Returns true for valid input', () => {
+    expect(isNull(null)).toBeTruthy();
+    expect(isNull(1)).toBeFalsy();
+  });
+});
+
+describe('isPrimitive()', () => {
+  it('Returns true for valid input', () => {
+    ['string', 1, BigInt(1), true, undefined, Symbol(), null].forEach(input => {
+      expect(isPrimitive(input)).toBeTruthy();
+    });
   });
 
   it('Returns false for invalid input', () => {
-    expect(isFiniteNumber(null)).toBeFalsy();
-    expect(isFiniteNumber(Infinity)).toBeFalsy();
+    [['test'], {}, Promise.resolve()].forEach(input => {
+      expect(isPrimitive(input)).toBeFalsy();
+    });
+  });
+});
+
+describe('isOptionalString()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalString('test')).toBeTruthy();
+    expect(isOptionalString(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalString(null)).toBeFalsy();
+    expect(isOptionalString(123)).toBeFalsy();
+  });
+});
+
+describe('isOptionalNumber()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalNumber(Math.PI)).toBeTruthy();
+    expect(isOptionalNumber(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalNumber(null)).toBeFalsy();
+    expect(isOptionalNumber('test')).toBeFalsy();
+  });
+});
+
+describe('isOptionalBigInt()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalBigInt(BigInt(1))).toBeTruthy();
+    expect(isOptionalBigInt(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalBigInt(null)).toBeFalsy();
+    expect(isOptionalBigInt('test')).toBeFalsy();
+  });
+});
+
+describe('isOptionalBoolean()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalBoolean(true)).toBeTruthy();
+    expect(isOptionalBoolean(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalBoolean(null)).toBeFalsy();
+    expect(isOptionalBoolean('test')).toBeFalsy();
+  });
+});
+
+describe('isOptionalSymbol()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalSymbol(Symbol())).toBeTruthy();
+    expect(isOptionalSymbol(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalSymbol(null)).toBeFalsy();
+    expect(isOptionalSymbol('test')).toBeFalsy();
+  });
+});
+
+describe('isOptionalNull()', () => {
+  it('Returns true for valid input', () => {
+    expect(isOptionalNull(null)).toBeTruthy();
+    expect(isOptionalNull(undefined)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isOptionalNull(true)).toBeFalsy();
+    expect(isOptionalNull('test')).toBeFalsy();
+  });
+});
+
+//
+
+describe('isStringWithLength()', () => {
+  it('Returns true for valid input', () => {
+    expect(isStringWithLength('test')).toBeTruthy();
+    expect(isStringWithLength('')).toBeFalsy();
+    expect(isStringWithLength(null)).toBeFalsy();
+  });
+});
+
+describe('isStringMatching()', () => {
+  it('Returns true for valid input', () => {
+    expect(isStringMatching('', /^$/)).toBeTruthy();
+    expect(isStringMatching('99', /^\d\d$/)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isStringMatching(null, /^null$/)).toBeFalsy();
+  });
+});
+
+describe('isNumericString()', () => {
+  it('Returns true for numeric strings', () => {
+    expect(isNumericString('12345')).toBeTruthy();
+    expect(isNumericString('3.14')).toBeTruthy();
+  });
+
+  it('Returns false for non-numeric strings', () => {
+    expect(isNumericString('abc')).toBeFalsy();
+  });
+});
+
+describe('isNumericValue()', () => {
+  it('Returns true for numeric strings', () => {
+    expect(isNumericValue('12345')).toBeTruthy();
+    expect(isNumericValue(Math.PI)).toBeTruthy();
+    expect(isNumericValue(BigInt(1))).toBeTruthy();
+  });
+
+  it('Returns false for non-numeric strings', () => {
+    expect(isNumericValue('abc')).toBeFalsy();
+  });
+});
+
+describe('isNumericValueArray()', () => {
+  it('Returns true for numeric strings', () => {
+    expect(isNumericValueArray(['12345', Math.PI, BigInt(1)])).toBeTruthy();
+  });
+
+  it('Returns false for non-numeric strings', () => {
+    expect(isNumericValueArray('abc')).toBeFalsy();
+    expect(isNumericValueArray(['xyz'])).toBeFalsy();
+  });
+});
+
+describe('isIntString()', () => {
+  it('Returns true for valid input', () => {
+    expect(isIntString('12345')).toBeTruthy();
+    expect(isIntString('-12345')).toBeTruthy();
+    expect(isIntString('+12345')).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isIntString('3.14')).toBeFalsy();
+    expect(isIntString(['xyz'])).toBeFalsy();
+  });
+});
+
+describe('isFiniteNumber()', () => {
+  it('Returns true for valid input', () => {
+    expect(isFiniteNumber(123)).toBeTruthy();
+    expect(isFiniteNumber(123.456)).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
     expect(isFiniteNumber(Number.NaN)).toBeFalsy();
+    expect(isFiniteNumber(Infinity)).toBeFalsy();
   });
 });
 
@@ -42,75 +288,17 @@ describe('isInteger()', () => {
   });
 });
 
-describe('isIntString()', () => {
+describe('isPositiveInteger()', () => {
   it('Returns true for valid inputs', () => {
-    ['123', '-123', '+123'].forEach(input => {
-      expect(isIntString(input)).toBeTruthy();
+    [0, -0, 1, 1000].forEach(input => {
+      expect(isPositiveInteger(input)).toBeTruthy();
     });
   });
 
   it('Returns false for invalid inputs', () => {
-    [Infinity, Number.NaN, 'bad input', false, null, {}, Math.PI].forEach(input => {
-      expect(isIntString(input)).toBeFalsy();
+    [-1000, -Infinity, Number.NaN, 'bad input', false, null, {}].forEach(input => {
+      expect(isPositiveInteger(input)).toBeFalsy();
     });
-  });
-});
-
-describe('isNumericString()', () => {
-  it('Returns true for numeric strings', () => {
-    expect(isNumericString('12345')).toBeTruthy();
-    expect(isNumericString('3.14')).toBeTruthy();
-  });
-
-  it('Returns false for non-numeric strings', () => {
-    expect(isNumericString('abc')).toBeFalsy();
-  });
-});
-
-describe('isNumericValue()', () => {
-  it('Returns true for numeric values', () => {
-    expect(isNumericValue('12345')).toBeTruthy();
-    expect(isNumericValue(Math.PI)).toBeTruthy();
-    expect(isNumericValue(123n)).toBeTruthy();
-  });
-
-  it('Returns false for non-numeric strings', () => {
-    expect(isNumericValue('abc')).toBeFalsy();
-  });
-});
-
-describe('isNumericValueArray()', () => {
-  it('Returns true for numeric strings', () => {
-    expect(isNumericValueArray(['123', Math.PI, 123n])).toBeTruthy();
-  });
-
-  it('Returns false for non-numeric strings', () => {
-    expect(isNumericValueArray(['abc'])).toBeFalsy();
-  });
-});
-
-describe('isOptionalNumber()', () => {
-  it('Returns true for valid input', () => {
-    expect(isOptionalNumber(Math.PI)).toBeTruthy();
-    expect(isOptionalNumber(undefined)).toBeTruthy();
-  });
-
-  it('Returns false for invalid input', () => {
-    expect(isOptionalNumber(null)).toBeFalsy();
-    expect(isOptionalNumber('test')).toBeFalsy();
-    expect(isOptionalNumber(Number.NaN)).toBeFalsy();
-  });
-});
-
-describe('isOptionalString()', () => {
-  it('Returns true for valid input', () => {
-    expect(isOptionalString('test')).toBeTruthy();
-    expect(isOptionalString(undefined)).toBeTruthy();
-  });
-
-  it('Returns false for invalid input', () => {
-    expect(isOptionalString(null)).toBeFalsy();
-    expect(isOptionalString(123)).toBeFalsy();
   });
 });
 
@@ -123,20 +311,6 @@ describe('isPositiveFiniteNumber()', () => {
   it('Returns false for invalid inputs', () => {
     [-123, Infinity, Number.NaN, 'bad input', false, null, {}].forEach(input => {
       expect(isPositiveFiniteNumber(input)).toBeFalsy();
-    });
-  });
-});
-
-describe('isPositiveInteger()', () => {
-  it('Returns true for valid inputs', () => {
-    [0, -0, 1, 1000].forEach(input => {
-      expect(isPositiveInteger(input)).toBeTruthy();
-    });
-  });
-
-  it('Returns false for invalid inputs', () => {
-    [-1000, -Infinity, Number.NaN, 'bad input', false, null, {}].forEach(input => {
-      expect(isPositiveInteger(input)).toBeFalsy();
     });
   });
 });
@@ -156,63 +330,61 @@ describe('isPropertyKey()', () => {
   });
 });
 
-describe('isStringMatching()', () => {
-  it('Returns true for valid input', () => {
-    expect(isStringMatching('', /^$/)).toBeTruthy();
-    expect(isStringMatching('99', /^\d\d$/)).toBeTruthy();
+describe('isObject()', () => {
+  it('Returns true for valid inputs', () => {
+    expect(isObject({})).toBeTruthy();
   });
 
-  it('Returns false for invalid input', () => {
-    expect(isStringMatching(null, /^null$/)).toBeFalsy();
+  it('Returns false for invalid inputs', () => {
+    expect(isObject('test')).toBeFalsy();
+    expect(isObject(null)).toBeFalsy();
   });
 });
 
 describe('isLengthAware()', () => {
-  it('Returns true for valid input', () => {
-    expect(isLengthAware({ length: 10 })).toBeTruthy();
+  it('Returns true for valid inputs', () => {
+    expect(isLengthAware([])).toBeTruthy();
+    expect(isLengthAware({ length: 1 })).toBeTruthy();
   });
 
-  it('Returns false for invalid input', () => {
-    expect(isLengthAware(null)).toBeFalsy();
-    expect(isLengthAware({})).toBeFalsy();
+  it('Returns false for invalid inputs', () => {
+    [-123, Infinity, Number.NaN, 'bad input', false, null, { pi: Math.PI }].forEach(input => {
+      expect(isLengthAware(input)).toBeFalsy();
+    });
   });
 });
 
-describe('isPrimitive()', () => {
-  it('Returns true for valid input', () => {
-    ['a', 1, 1n, true, undefined, Symbol(), null].forEach(input => {
-      expect(isPrimitive(input)).toBeTruthy();
-    });
-  });
-
-  it('Returns false for invalid input', () => {
-    expect(isPrimitive({})).toBeFalsy();
-    expect(isPrimitive(new Date())).toBeFalsy();
-  });
-});
-
-describe('isObject()', () => {
-  it('Returns true for valid input', () => {
-    [new Date(), {}].forEach(input => {
-      expect(isObject(input)).toBeTruthy();
-    });
-  });
-
-  it('Returns false for invalid input', () => {
-    ['a', 1, 1n, true, undefined, Symbol(), null].forEach(input => {
-      expect(isObject(input)).toBeFalsy();
-    });
-  });
-
-  it('Can customize the `is` return type', () => {
-    type Demo = {
-      demo: true;
-    };
-
+describe('isStringRecord()', () => {
+  it('Returns true for valid inputs', () => {
     expect(
-      isObject<Demo>({
-        demo: true,
+      isStringRecord({
+        test: 'true',
+        enabled: 'false',
       }),
     ).toBeTruthy();
+  });
+
+  it('Returns false for invalid inputs', () => {
+    [-123, Infinity, Number.NaN, 'bad input', false, null, { pi: Math.PI }].forEach(input => {
+      expect(isStringRecord(input)).toBeFalsy();
+    });
+  });
+});
+
+describe('isPromiseFulfilledResult()', () => {
+  it('Returns true for valid inputs', async () => {
+    const result = await Promise.allSettled([Promise.resolve(), Promise.reject(new Error('test'))]);
+
+    expect(isPromiseFulfilledResult(result[0])).toBeTruthy();
+    expect(isPromiseFulfilledResult(result[1])).toBeFalsy();
+  });
+});
+
+describe('isPromiseRejectedResult()', () => {
+  it('Returns true for valid inputs', async () => {
+    const result = await Promise.allSettled([Promise.resolve(), Promise.reject(new Error('test'))]);
+
+    expect(isPromiseRejectedResult(result[0])).toBeFalsy();
+    expect(isPromiseRejectedResult(result[1])).toBeTruthy();
   });
 });
