38c8b9a929cbc06e6523f6f4fa4fd02f31bb56b0
Eric King <eric@webdeveric.com>
2023-01-20 11:35:18 -0600

    feat: added `isNonNullable()`

--- test/type-predicate.test.ts
+++ test/type-predicate.test.ts
@@ -10,6 +10,7 @@ import {
   isInteger,
   isIntString,
   isLengthAware,
+  isNonNullable,
   isNull,
   isNullArray,
   isNumber,
@@ -371,6 +372,23 @@ describe('isPositiveFiniteNumber()', () => {
     [-123, Infinity, Number.NaN, 'bad input', false, null, {}].forEach(input => {
       expect(isPositiveFiniteNumber(input)).toBeFalsy();
     });
+  });
+});
+
+describe('isNonNullable()', () => {
+  it('Returns true for valid input', () => {
+    expect(isNonNullable('string-key')).toBeTruthy();
+    expect(isNonNullable(123)).toBeTruthy();
+    expect(isNonNullable(Symbol('symbol-key'))).toBeTruthy();
+  });
+
+  it('Returns false for invalid input', () => {
+    expect(isNonNullable(null)).toBeFalsy();
+    expect(isNonNullable(undefined)).toBeFalsy();
+  });
+
+  it('Can be used with Array.filter()', () => {
+    expect([1, null, 2, undefined, 3].filter(isNonNullable)).toEqual([1, 2, 3]);
   });
 });
 
