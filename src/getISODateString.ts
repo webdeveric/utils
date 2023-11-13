16ceb3f96ae320ed06f83019254f082448da10b9
Eric King <eric@webdeveric.com>
2023-11-13 12:28:01 -0600

    chore: moved type

--- src/getISODateString.ts
+++ src/getISODateString.ts
@@ -1,7 +1,4 @@
-import type { Branded } from './types/branded.js';
-import type { DateString, TimeString } from './types/date-time.js';
-
-export type ISODateString = Branded<`${DateString}T${TimeString}.${number}Z`, 'ISODateString'>;
+import type { ISODateString } from './types/date-time.js';
 
 export const getISODateString = (value: ConstructorParameters<typeof Date>[0] = new Date()): ISODateString =>
   (value instanceof Date ? value : new Date(value)).toISOString() as ISODateString;
--- src/types/date-time.ts
+++ src/types/date-time.ts
@@ -1,3 +1,7 @@
+import type { Branded } from './branded.js';
+
 export type DateString = `${number}-${number}-${number}`;
 
 export type TimeString = `${number}:${number}:${number}`;
+
+export type ISODateString = Branded<`${DateString}T${TimeString}.${number}Z`, 'ISODateString'>;
