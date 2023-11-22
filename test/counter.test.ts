05afb1e8bad459d3b423407c17c20948d2d7a634
Eric King <eric@webdeveric.com>
2023-11-22 11:46:42 -0600

    chore: added `counter()`

---
+++ test/counter.test.ts
@@ -0,0 +1,20 @@
+import { isGeneratorFunction, isGeneratorObject } from 'node:util/types';
+
+import { describe, expect, it } from 'vitest';
+
+import { counter } from '../src/counter.js';
+describe('counter()', () => {
+  it('Is a GeneratorFunction', () => {
+    expect(isGeneratorFunction(counter)).toBeTruthy();
+  });
+
+  it('Returns a Generator', () => {
+    expect(isGeneratorObject(counter())).toBeTruthy();
+  });
+
+  it('Can provide start and end', () => {
+    const generator = counter(10, 12);
+
+    expect([...generator]).toEqual([10, 11, 12]);
+  });
+});
