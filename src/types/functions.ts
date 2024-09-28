00ed2307f2637c9e72250a70f7c8f8ae605b3b76
Eric King <eric@webdeveric.com>
2024-09-28 17:45:18 -0500

    chore(types): added `InferPredicatesReturnType`

--- src/types/functions.ts
+++ src/types/functions.ts
@@ -7,3 +7,7 @@ export type TypePredicateFn<T> = (input: unknown) => input is T;
 export type TypeAssertionFn<T> = (input: unknown) => asserts input is T;
 
 export type CompareFn<T> = (a: T, b: T) => number;
+
+export type InferPredicatesReturnType<Predicates extends TypePredicateFn<unknown>[]> = {
+  [K in keyof Predicates]: Predicates[K] extends TypePredicateFn<infer T> ? T : never;
+}[number];
