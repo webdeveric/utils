ea72c734bc1f23f4578d0ecd7378ba34b05da4ef
Eric King <eric@webdeveric.com>
2024-11-28 12:07:01 -0600

    refactor: use `exec()` instead of `match()`

--- src/getPackageName.ts
+++ src/getPackageName.ts
@@ -1,3 +1,3 @@
 export function getPackageName(input: string): string | undefined {
-  return input.match(/^(?<pkg>(?:(?<scope>@[^/]+)\/)?(?<name>[a-z][^/]+))/i)?.groups?.pkg;
+  return /^(?<pkg>(?:(?<scope>@[^/]+)\/)?(?<name>[a-z][^/]+))/i.exec(input)?.groups?.['pkg'];
 }
