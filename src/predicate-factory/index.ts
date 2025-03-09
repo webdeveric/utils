df3a80de809766070999ca489e4a1dffe9ec3390
Eric King <eric@webdeveric.com>
2025-03-08 23:22:09 -0600

    feat: added `is()`

--- src/predicate-factory/index.ts
+++ src/predicate-factory/index.ts
@@ -8,6 +8,7 @@ export * from './createObjectShapePredicate.js';
 export * from './createStringLengthRangePredicate.js';
 export * from './createStringMatchingPredicate.js';
 export * from './everyItem.js';
+export * from './is.js';
 export * from './maybeArray.js';
 export * from './maybeNull.js';
 export * from './maybeUndefined.js';
