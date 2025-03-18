2e24940a5174173d7de2873726b8760f2bf56fed
Eric King <eric@webdeveric.com>
2025-03-18 17:49:40 -0500

    fix: use more correct type parameter

--- src/predicate/factory/fromEnum.ts
+++ src/predicate/factory/fromEnum.ts
@@ -1,10 +1,10 @@
 import type { TypePredicateFn } from '../../types/functions.js';
-import type { UnknownRecord } from '../../types/records.js';
+import type { EnumRecord } from '../../types/records.js';
 
 /**
  * @internal
  */
-const getEnumValues = <T extends UnknownRecord>(enumObject: T): Set<unknown> => {
+const getEnumValues = <T extends EnumRecord>(enumObject: T): Set<unknown> => {
   const entries = Object.entries(enumObject);
 
   const ignoreKeys = entries.reduce((keys, [, value]) => {
@@ -27,7 +27,7 @@ const getEnumValues = <T extends UnknownRecord>(enumObject: T): Set<unknown> => 
 /**
  * Create a type predicate function that checks if the input is a member of the TypeScript `enum`
  */
-export const fromEnum = <T extends UnknownRecord>(enumObject: T): TypePredicateFn<T[keyof T]> => {
+export const fromEnum = <T extends EnumRecord>(enumObject: T): TypePredicateFn<T[keyof T]> => {
   const values = getEnumValues(enumObject);
 
   return (input: unknown): input is T[keyof T] => values.has(input);
