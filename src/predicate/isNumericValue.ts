6119716185f7690d5ac6fbea9d12de4a66658d96
Eric King <eric@webdeveric.com>
2025-03-23 12:43:25 -0500

    fix: check for `NaN`

--- src/predicate/isNumericValue.ts
+++ src/predicate/isNumericValue.ts
@@ -3,4 +3,4 @@ import { isNumericString } from './isNumericString.js';
 import type { NumericValue } from '../types/common.js';
 
 export const isNumericValue = (input: unknown): input is NumericValue =>
-  typeof input === 'number' || typeof input === 'bigint' || isNumericString(input);
+  (typeof input === 'number' && !Number.isNaN(input)) || typeof input === 'bigint' || isNumericString(input);
