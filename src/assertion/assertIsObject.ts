0ee1e70a4cef262475fd6e466406756fbaa2ec49
Eric King <eric@webdeveric.com>
2025-03-24 15:52:30 -0500

    fix: updated type assertion signature

--- src/assertion/assertIsObject.ts
+++ src/assertion/assertIsObject.ts
@@ -2,10 +2,12 @@ import { isObject } from '../predicate/isObject.js';
 
 import { getError } from './getError.js';
 
+import type { UnknownRecord } from '../types/records.js';
+
 export function assertIsObject(
   input: unknown,
   error: string | Error = 'input is not an object',
-): asserts input is object {
+): asserts input is UnknownRecord {
   if (!isObject(input)) {
     throw getError(error);
   }
