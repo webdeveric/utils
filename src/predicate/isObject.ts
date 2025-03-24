42bea43d1b638cccf0b0dfeae58da9735009ce36
Eric King <eric@webdeveric.com>
2025-03-24 15:29:45 -0500

    fix: updated type predicate signature

--- src/predicate/isObject.ts
+++ src/predicate/isObject.ts
@@ -1,5 +1,7 @@
+import type { UnknownRecord } from '../types/records.js';
+
 /**
  * Determine if `input` is a non-null object and not an array.
  */
-export const isObject = (input: unknown): input is object =>
+export const isObject = (input: unknown): input is UnknownRecord =>
   input !== null && typeof input === 'object' && !Array.isArray(input);
