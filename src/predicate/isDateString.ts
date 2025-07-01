be55dfaf122b800a4d78d33b615753ca089f971a
Eric King <eric@webdeveric.com>
2025-07-01 10:51:50 -0500

    refactor: use `Date.parse()`

--- src/predicate/isDateString.ts
+++ src/predicate/isDateString.ts
@@ -4,4 +4,4 @@ import type { TypePredicateFn } from '../types/functions.js';
  * Determine if `input` is a valid date string.
  */
 export const isDateString: TypePredicateFn<string> = (input: unknown): input is string =>
-  typeof input === 'string' && !Number.isNaN(new Date(input).getTime());
+  typeof input === 'string' && !Number.isNaN(Date.parse(input));
