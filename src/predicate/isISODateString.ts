392ef758a55f1944c4740e7a612f3928daa6ce4c
Eric King <eric@webdeveric.com>
2025-08-15 13:39:14 -0500

    refactor: allow higher precision timezone

--- src/predicate/isISODateString.ts
+++ src/predicate/isISODateString.ts
@@ -3,7 +3,7 @@ import { matching } from './factory/matching.js';
 import type { ISODateString } from '../types/date-time.js';
 
 export const ISO_DATE_PATTERN =
-  /^(?<year>\d{4,})-(?<month>[01]\d)-(?<day>0[1-9]|[12]\d|3[01])T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})\.(?<timezone>\d{3}Z)$/;
+  /^(?<year>\d{4,})-(?<month>[01]\d)-(?<day>0[1-9]|[12]\d|3[01])T(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})\.(?<timezone>\d+Z)$/;
 
 /**
  * This checks to see if the input matches the format that `toISOString()`
--- test/type-predicate.test.ts
+++ test/type-predicate.test.ts
@@ -11,7 +11,6 @@ import {
   isFunction,
   isInteger,
   isIntString,
-  isISODateString,
   isIterable,
   isLengthAware,
   isNull,
@@ -320,19 +319,6 @@ describe('isIntString()', () => {
   it('Returns false for invalid input', () => {
     expect(isIntString('3.14')).toBeFalsy();
     expect(isIntString(['xyz'])).toBeFalsy();
-  });
-});
-
-describe('isISODateString()', () => {
-  it.each([new Date(0).toISOString(), new Date('+9999-12-31').toISOString()])(
-    'Returns true for valid input: "%s"',
-    (input) => {
-      expect(isISODateString(input)).toBeTruthy();
-    },
-  );
-
-  it.each(['9999-12-31T06:00:00', null, false, ''])('Returns false for invalid input: "%s"', (input) => {
-    expect(isISODateString(input)).toBeFalsy();
   });
 });
 
