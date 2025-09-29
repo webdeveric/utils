9bfcfc760d70e67ac95c821f79a03093c8549440
Eric King <eric@webdeveric.com>
2025-09-29 09:51:30 -0500

    chore: avoid modulo bias

--- src/sort/byRandom.ts
+++ src/sort/byRandom.ts
@@ -1,4 +1,12 @@
 /**
  * Randomly return -1, 0, or 1. This is an easy way to shuffle an array.
  */
-export const byRandom = (): number => (crypto.getRandomValues(new Uint8Array(1))[0]! % 3) - 1;
+export const byRandom = (): number => {
+  let value: number;
+
+  do {
+    value = crypto.getRandomValues(new Uint8Array(1))[0]!;
+  } while (value >= 252); // Avoid modulo bias
+
+  return (value % 3) - 1;
+};
