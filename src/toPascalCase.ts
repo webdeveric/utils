468aad06952b5fde15cbe79ce94344eb109dfb5e
Eric King <eric@webdeveric.com>
2025-10-26 10:39:17 -0500

    chore: use `typeof` instead of `getType()`

--- src/toPascalCase.ts
+++ src/toPascalCase.ts
@@ -1,5 +1,4 @@
 import { capitalize } from './capitalize.js';
-import { getType } from './getType.js';
 
 import type { StringRecord } from './types/records.js';
 import type { PascalCase } from './types/strings.js';
@@ -10,7 +9,7 @@ export function toPascalCase<Type extends string>(text: Type, customWords?: Stri
 
   let fixedWords = words?.map<string>((word) => capitalize(word.replace(/'/g, '')));
 
-  if (fixedWords && customWords && getType(customWords) === 'Object') {
+  if (fixedWords && customWords && typeof customWords === 'object') {
     const replacements = new Map(
       Object.entries(customWords).map<KeyValueTuple<string, string>>(([key, value]) => [key.toLowerCase(), value]),
     );
