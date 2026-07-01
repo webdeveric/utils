14066b0357096dcc7fcd34ceb92c0c4858244761
Eric King <eric@webdeveric.com>
2026-07-01 14:41:18 -0500

    chore: consistent `trim()`

--- src/trimIndentation.ts
+++ src/trimIndentation.ts
@@ -5,5 +5,5 @@ import { stripIndent } from './stripIndent.js';
 export function trimIndentation(input: string): string {
   const whiteSpace = findIndentation(input.split(new RegExp(NEWLINE_RAW_PATTERN)));
 
-  return whiteSpace ? stripIndent(input, whiteSpace).trim() : input;
+  return (whiteSpace ? stripIndent(input, whiteSpace) : input).trim();
 }
